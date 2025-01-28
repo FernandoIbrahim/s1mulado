4cde7571b5f3fd40201b6afc085636f78b375f8f
Fernando Antônio Ferreira Ibrahim <80763509+FernandoIbrahim@users.noreply.github.com>
2025-01-28 16:06:46 -0300

    Configurando CORS spring security

--- Codigo/back/s1mulado/src/main/java/com/example/S1mulado/infra/security/SecurityConfigurations.java
+++ Codigo/back/s1mulado/src/main/java/com/example/S1mulado/infra/security/SecurityConfigurations.java
@@ -13,6 +13,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +46,19 @@ public class SecurityConfigurations {
     }
 
     @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 
         return authenticationConfiguration.getAuthenticationManager();
@@ -51,5 +71,7 @@ public class SecurityConfigurations {
         return new BCryptPasswordEncoder();
 
     }
+
+
 
 }
